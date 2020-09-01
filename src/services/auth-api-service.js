9e4b6b79dae75b1c594071079e58021d1d97be67
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-09-01 15:11:12 -0700

    added additional guiding comments

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,6 +1,7 @@
 import config from '../config';
 
 const AuthApiService = {
+    // client-side POST to handle user login requests
     postLogin({user_name, password}) {
         return fetch(`${config.API_ENDPOINT}/auth/login`, {
             method: "POST",
@@ -15,6 +16,7 @@ const AuthApiService = {
                     : res.json()
             });
     },
+    // client-side POST to handle registration requests
     postUser(user) {
         return fetch(`${config.API_ENDPOINT}/users`, {
             method: "POST",
