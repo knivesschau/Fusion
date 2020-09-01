884417e13f9f13fa17ffec62fde78d77a7bb11e1
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-09-01 14:12:54 -0700

    added production heroku server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://afternoon-anchorage-79162.herokuapp.com/api',
     TOKEN_KEY: 'fusion-client-auth-token',
 };
